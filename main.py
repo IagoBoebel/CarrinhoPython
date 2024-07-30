ccdc554f12353363e8994d0ef046ab38eba817b4
Iago Boebel <iagogtr@gmail.com>
2024-07-30 11:26:17 -0300

    Nova Frase

---
+++ main.py
@@ -0,0 +1,10 @@
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input("Digite um número: "))
+
+if numero_1 <= numero_2:
+    print("O número 2 é maior")
+else:
+    print("O núemro 1 é maior")
+
+print("Obrigado por usar o programa: ")
